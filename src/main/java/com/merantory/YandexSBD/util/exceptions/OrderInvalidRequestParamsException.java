79a3a7231e8f0412f17b48ca630c6deffc53b972
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-22 21:59:16 +0300

    Order invalid request params exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/OrderInvalidRequestParamsException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions;
+
+public class OrderInvalidRequestParamsException extends RuntimeException {
+    public OrderInvalidRequestParamsException(String msg) {
+        super(msg);
+    }
+}
