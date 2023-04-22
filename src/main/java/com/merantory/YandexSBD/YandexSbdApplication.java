643c160e768bc5c44fb6b0d786928c4b030e9c9a
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-22 21:57:00 +0300

    application start point added

---
+++ src/main/java/com/merantory/YandexSBD/YandexSbdApplication.java
@@ -0,0 +1,13 @@
+package com.merantory.YandexSBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexSbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexSbdApplication.class, args);
+	}
+
+}
