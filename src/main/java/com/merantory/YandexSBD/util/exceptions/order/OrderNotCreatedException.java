32c90df3034edbaf4f0e5b211365ad20f81e1c99
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-29 09:41:17 +0300

    Order not created exception updated

--- src/main/java/com/merantory/YandexSBD/util/exceptions/order/OrderNotCreatedException.java
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/order/OrderNotCreatedException.java
@@ -1,6 +1,6 @@
-package com.merantory.YandexSBD.util.exceptions;
+package com.merantory.YandexSBD.util.exceptions.order;
 
-public class OrderNotCreatedException extends RuntimeException {
+public class OrderNotCreatedException extends OrderMainException {
     public OrderNotCreatedException(String msg) {
         super(msg);
     }
