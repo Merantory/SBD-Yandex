d125f1f22526bd85bdf778a4ab30f466d5bdb3b6
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-30 02:52:45 +0300

    Courier invalid request params exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/courier/CourierInvalidRequestParamsException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions.courier;
+
+public class CourierInvalidRequestParamsException extends CourierMainException {
+    public CourierInvalidRequestParamsException(String msg) {
+        super(msg);
+    }
+}
