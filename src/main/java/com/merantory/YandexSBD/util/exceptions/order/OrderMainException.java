2d064dfaf7cf43dd05a1257dc73da205853b84c5
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-29 09:40:37 +0300

    Order main exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/order/OrderMainException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions.order;
+
+public class OrderMainException extends RuntimeException {
+    OrderMainException(String msg) {
+        super(msg);
+    }
+}
