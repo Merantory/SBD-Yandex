8ddd27a71d166de402b11842c6bfbb8d4187835c
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-29 09:42:04 +0300

    Order not mark complete exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/order/OrderNotMarkCompleteException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions.order;
+
+public class OrderNotMarkCompleteException extends OrderMainException {
+    public OrderNotMarkCompleteException(String msg) {
+        super(msg);
+    }
+}
