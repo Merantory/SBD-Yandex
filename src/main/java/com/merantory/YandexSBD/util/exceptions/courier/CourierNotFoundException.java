10666ed7978d522cc276620ae4989bd7b31619fc
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-30 02:53:19 +0300

    Courier not found exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/courier/CourierNotFoundException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions.courier;
+
+public class CourierNotFoundException extends CourierMainException {
+    public CourierNotFoundException(String msg) {
+        super(msg);
+    }
+}
