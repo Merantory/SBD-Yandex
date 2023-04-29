cca25a5b6c2c631d1d9d4b346d0bc2604fdba7a1
Merantory <59146779+Merantory@users.noreply.github.com>
2023-04-30 02:53:06 +0300

    Courier meta info invalid date exception added

---
+++ src/main/java/com/merantory/YandexSBD/util/exceptions/courier/CourierMetaInfoInvalidDateException.java
@@ -0,0 +1,7 @@
+package com.merantory.YandexSBD.util.exceptions.courier;
+
+public class CourierMetaInfoInvalidDateException extends CourierMainException {
+    public CourierMetaInfoInvalidDateException (String msg) {
+        super(msg);
+    }
+}
