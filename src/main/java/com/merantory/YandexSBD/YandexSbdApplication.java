3cb49e17580227c177337d45be00ec0bfa378d12
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-02 14:45:55 +0300

    Delete unused imports

--- src/main/java/com/merantory/YandexSBD/YandexSbdApplication.java
+++ src/main/java/com/merantory/YandexSBD/YandexSbdApplication.java
@@ -2,12 +2,12 @@ package com.merantory.YandexSBD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class YandexSbdApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(YandexSbdApplication.class, args);
 	}
-
 }
