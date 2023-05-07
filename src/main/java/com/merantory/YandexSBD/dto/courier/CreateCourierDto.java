4c698c149861b3dc87fc22f01eac0f003a513f31
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-07 23:10:40 +0300

    Rename regions json property

--- src/main/java/com/merantory/YandexSBD/dto/courier/CreateCourierDto.java
+++ src/main/java/com/merantory/YandexSBD/dto/courier/CreateCourierDto.java
@@ -1,7 +1,7 @@
 package com.merantory.YandexSBD.dto.courier;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.merantory.YandexSBD.models.CourierType;
+import com.merantory.YandexSBD.models.CourierTypeEnum;
 import com.merantory.YandexSBD.util.validators.TimeInterval;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
@@ -16,9 +16,10 @@ import java.util.Set;
 @Setter
 public class CreateCourierDto {
     @JsonProperty("courier_type")
-    private CourierType courierType;
+    private CourierTypeEnum courierType;
 
     @NotEmpty(message = "should not be empty")
+    @JsonProperty("regions")
     private Set<@Positive(message = "values should be positive") Long> regions;
 
     @JsonProperty("working_hours")
