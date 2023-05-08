79525f5f9661ee62f79c5701725c6495af8668e7
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-08 19:14:05 +0300

    Order controller created

---
+++ src/main/java/com/merantory/YandexSBD/controllers/OrderController.java
@@ -0,0 +1,11 @@
+package com.merantory.YandexSBD.controllers;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+}
