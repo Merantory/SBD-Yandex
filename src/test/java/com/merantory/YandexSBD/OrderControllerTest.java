5bf25e1a959cb2014164fcdfc0347005f99c9ff7
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-09 17:23:51 +0300

    OrderController Test before each test setup meathod added

--- src/test/java/com/merantory/YandexSBD/OrderControllerTest.java
+++ src/test/java/com/merantory/YandexSBD/OrderControllerTest.java
@@ -1,14 +1,18 @@
 package com.merantory.YandexSBD;
 
+import com.merantory.YandexSBD.controllers.CustomGlobalExceptionHandler;
 import com.merantory.YandexSBD.controllers.OrderController;
 import com.merantory.YandexSBD.services.OrderService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -20,4 +24,13 @@ class OrderControllerTest {
 
     @Mock
     private OrderService orderService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        orderController = new OrderController(orderService);
+        mockMvc = MockMvcBuilders.standaloneSetup(orderController)
+                .setControllerAdvice(CustomGlobalExceptionHandler.class)
+                .build();
+    }
 }
