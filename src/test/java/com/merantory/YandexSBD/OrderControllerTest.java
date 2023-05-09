eebf6440ab80817eec24b510879f9df1d0da8d3e
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-09 17:24:37 +0300

    OrderController get orders list test added

--- src/test/java/com/merantory/YandexSBD/OrderControllerTest.java
+++ src/test/java/com/merantory/YandexSBD/OrderControllerTest.java
@@ -1,18 +1,41 @@
 package com.merantory.YandexSBD;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.merantory.YandexSBD.controllers.CustomGlobalExceptionHandler;
 import com.merantory.YandexSBD.controllers.OrderController;
+import com.merantory.YandexSBD.dto.order.CompleteOrderDto;
+import com.merantory.YandexSBD.dto.order.CreateOrderDto;
+import com.merantory.YandexSBD.dto.order.OrderDto;
+import com.merantory.YandexSBD.dto.order.requests.RequestCompleteOrderDto;
+import com.merantory.YandexSBD.dto.order.requests.RequestCreateOrder;
+import com.merantory.YandexSBD.models.Order;
 import com.merantory.YandexSBD.services.OrderService;
+import com.merantory.YandexSBD.util.exceptions.order.OrderInvalidRequestParamsException;
+import com.merantory.YandexSBD.util.exceptions.order.OrderNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -32,5 +55,18 @@ class OrderControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(orderController)
                 .setControllerAdvice(CustomGlobalExceptionHandler.class)
                 .build();
+    }
+
+    @Test
+    public void getOrdersListTest() throws Exception {
+        int defaultOffset = 0;
+        int defaultLimit = 1;
+
+        mockMvc.perform(get("/orders")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+        verify(orderService, times(1)).getOrdersList(defaultOffset, defaultLimit);
     }
 }
