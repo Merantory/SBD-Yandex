352ce005eed732a84ffd6b5fd92f5b91eaff5a82
Merantory <59146779+Merantory@users.noreply.github.com>
2023-05-10 23:28:30 +0300

    CourierController get couriers list method added

--- src/main/java/com/merantory/YandexSBD/controllers/CourierController.java
+++ src/main/java/com/merantory/YandexSBD/controllers/CourierController.java
@@ -1,10 +1,21 @@
 package com.merantory.YandexSBD.controllers;
 
+import com.merantory.YandexSBD.dto.courier.CourierConverter;
+import com.merantory.YandexSBD.dto.courier.CourierDto;
+import com.merantory.YandexSBD.dto.courier.responses.ResponseCourierDto;
+import com.merantory.YandexSBD.models.Courier;
 import com.merantory.YandexSBD.services.CourierService;
+import com.merantory.YandexSBD.util.exceptions.courier.CourierInvalidRequestParamsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Validated
 @RestController
@@ -15,5 +26,21 @@ public class CourierController {
     @Autowired
     public CourierController(CourierService courierService) {
         this.courierService = courierService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseCourierDto> getCouriersList(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                                              @RequestParam(value = "limit", defaultValue = "1") int limit) {
+        if (offset < 0 || limit < 1) {
+            String errorMessage = "Invalid query params given. offset = " + offset + " limit = " + limit;
+            throw new CourierInvalidRequestParamsException(errorMessage);
+        }
+
+
+        List<Courier> courierList = courierService.getCouriersList(offset, limit);
+        List<CourierDto> courierDtoList = CourierConverter.convertCourierListToCourierDtoList(courierList);
+        ResponseCourierDto responseCourierDto = new ResponseCourierDto(courierDtoList, offset, limit);
+
+        return new ResponseEntity<>(responseCourierDto, HttpStatus.OK);
     }
 }
